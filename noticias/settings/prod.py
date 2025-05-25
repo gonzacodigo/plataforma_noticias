a97915764119017aaf8199f5d6e1a154b9420946
Gonzalo Arce <gonza_arc@hotmail.com>
2025-05-25 01:42:29 -0300

    se soluciono http host

--- noticias/settings/prod.py
+++ noticias/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['142.93.69.64', 'localhost', '127.0.0.1', 'wwww.gondigital.pro', 'gondigital.pro']
+ALLOWED_HOSTS = ['*']
 
 
 # Database
